486f6fb9f29fa9d79f87bda95c0487d509b4a323
Caio Costa <caiopaulinocosta@hotmail.com>
2024-03-14 01:02:52 -0300

    Melhorando HTML

--- EX02/produto.php
+++ EX02/produto.php
@@ -15,16 +15,22 @@ class ProdutoManager {
         
         $produtoData = $query->fetch(PDO::FETCH_ASSOC);
         if (!$produtoData) {
-            return false;
+            return false; 
         }
         
         return $produtoData;
     }
-
 }
 
 $produtoManager = new ProdutoManager();
 $produtoData = $produtoManager->getProdutoById(1);
+
+if ($produtoData !== false) {
+    $produtoId = $produtoData['id'];
+    $produtoNome = $produtoData['produtoNome'];
+    $produtoDescricao = $produtoData['produtoDescricao'];
+    $produtoPreco = $produtoData['produtoPreco'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -32,19 +38,33 @@ $produtoData = $produtoManager->getProdutoById(1);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Produtos</title>
 </head>
 <body>
 
-<?php if ($produtoData !== false): ?>
-    <ul>
-        <?php foreach ($produtoData as $key => $value): ?>
-            <li><?php echo $key . ': ' . $value; ?></li>
-        <?php endforeach; ?>
-    </ul>
-<?php else: ?>
-    <p>Nenhum produto encontrado.</p>
-<?php endif; ?>
+
+<div class="container">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nome</th>
+      <th scope="col">Descrição</th>
+      <th scope="col">Preço</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><?= $produtoId ?></td>
+      <td><?= $produtoNome ?></td>
+      <td><?= $produtoDescricao ?></td>
+      <td><?= $produtoPreco ?></td>
+    </tr>
+  </tbody>
+</table>
+</div>
 
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
